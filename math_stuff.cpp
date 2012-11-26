ade08daace767a27458b7a2321ba8fe07be61ae2
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-11-26 22:51:41 +0100

    fix fibonacci

--- math_stuff.cpp
+++ math_stuff.cpp
@@ -9,17 +9,17 @@ LL fib(LL n, LL m) {
   n -= 1;
   while (n) {
     if (n & 1) {
-      a2=(a*a1+b*c1)%m;
-      b2=(a*b1 + b*d1)%m;
-      c2=(c*a1 + d*c1)%m;
-      d2=(c*b1 + d*d1)%m;
+      a2=(a*a1%m + b*c1%m)%m;
+      b2=(a*b1%m + b*d1%m)%m;
+      c2=(c*a1%m + d*c1%m)%m;
+      d2=(c*b1%m + d*d1%m)%m;
       a=a2;b=b2;c=c2;d=d2;
     }
     n >>= 1;
-    a2=(a1*a1 + b1*c1)%m;
-    b2=(a1*b1 + b1*d1)%m;
-    c2=(c1*a1 + d1*c1)%m;
-    d2=(c1*b1 + d1*d1)%m;
+    a2=(a1*a1%m + b1*c1%m)%m;
+    b2=(a1*b1%m + b1*d1%m)%m;
+    c2=(c1*a1%m + d1*c1%m)%m;
+    d2=(c1*b1%m + d1*d1%m)%m;
     a1=a2;b1=b2;c1=c2;d1=d2;
   }
   return a;

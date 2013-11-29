89162faf9991deded7c6635f853e6cefe331f677
Niklas Baumstark <niklas.baumstark@gmail.com>
2013-11-29 18:03:21 +0100

    add dlog and primitive root

--- math_stuff.cpp
+++ math_stuff.cpp
@@ -25,3 +25,40 @@ LL fib(LL n, LL m) {
   return a;
 }
 
+ll p;
+ll powmod(ll x, ll e) {
+  if (e == 0) return 1;
+  if (e & 1) return x * powmod(x, e - 1) % p;
+  ll y = powmod(x, e / 2);
+  return y * y % p;
+}
+
+// factors_p1 = prime factors of (p - 1)
+bool is_primitive(ll g) {
+  for (auto q : factors_p1) {
+    if (1 == powmod(g, (p-1)/q)) return 0;
+  }
+  return 1;
+}
+ll g; // g = primitive root of p
+void find_prim() {
+  for (;;) {
+    g = (((ll)rand() << 15) | (ll)rand()) % p;
+    if (g < 2) continue;
+    if (is_primitive(g)) return;
+  }
+}
+bool have_powers = 0;
+unordered_map<ll,ll> powers;
+ll dlog(ll b) { // find x such that g^x = b (mod p)
+  ll m = (ll)(ceil(sqrt(p-1))+0.5); // better use binary search here...
+  if (!have_powers) rep(j,0,m) powers[powmod(g, j)] = j;
+  have_powers = 1;
+  ll gm = powmod(g, -m + 2*(p-1));
+  rep(i,0,m) {
+    if (contains(powers, b)) return i*m + powers[b];
+    b = b * gm % p;
+  }
+  assert(0);
+  return -1;
+}

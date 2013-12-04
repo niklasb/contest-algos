b4c5f331c445e175198d734d9bb127a0d7fcce3c
Niklas Baumstark <niklas.baumstark@gmail.com>
2013-12-04 15:23:44 +0100

    update fenwick tree

--- fennwick_tree.cpp
+++ fennwick_tree.cpp
@@ -1,19 +1,27 @@
-// represents fennwick tree over interval 1..n (n incl.)
-int t[maxn];
-int n;
+const int n = 10;
 
-void update(int idx, int x) {
-  while (idx <= n) {
-    t[idx] += x;
-    idx += idx & -idx;
-  }
+void update_idx(int tree[], int idx, int val) { // v[idx] += val
+  for (; idx <= n; idx += idx & -idx) tree[idx] += val;
+}
+int read_prefix(int tree[], int idx) { // get sum v[1..idx]
+  int sum = 0;
+  for (; idx > 0; idx -= idx & -idx) sum += tree[idx];
+  return sum;
+}
+void update_prefix(int tree[], int idx, int val) { // v[1..idx] += val
+  for (; idx > 0; idx -= idx & -idx) tree[idx] += val;
+}
+int read_idx(int tree[], int idx) { // get v[idx]
+  int sum = 0;
+  for (; idx <= n; idx += idx & -idx) sum += tree[idx];
+  return sum;
 }
 
-int read(int idx) {
-  int sum = 0;
-  while (idx > 0) {
-    sum += t[idx];
-    idx -= idx & -idx;
-  }
-  return sum;
+#include <iostream>
+using namespace std;
+int tree[n+1];
+int main() {
+  update_prefix(tree, 3, 1);
+  update_prefix(tree, 5, 3);
+  for (int i = 1; i <= n; ++ i) cout << read_idx(tree, i) << " ";cout<<endl;
 }

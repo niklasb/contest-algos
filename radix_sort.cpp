a9c6188770ebd751971d3201a01819b6f7094d13
Niklas Baumstark <niklas.baumstark@gmail.com>
2014-01-26 23:24:01 -0500

    radix sort + BB[alpha] tree

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,27 +1,30 @@
 // works in-place, O(n*d), swap needs to be adapted
 // could contain a bug! test against sort first!
-// len = length of element strings
+//
+// len = number of digits
+// getdig(x, 0) should get the most significant digit, get(x, len - 1) the lsd etc.
 const int max_val=256;
-void radix_sort_msb(unsigned char seq[][], int len, int l, int r, int base = 0) {
+template<typename T, typename F>
+void radix_sort_msb(T& seq, F getdig, int len, int l, int r, int base = 0) {
   if (r-l <= 20) {
-    sort(seq+l,seq+r); // even better: use insertion sort
+    sort(&seq[l],&seq[r]); // even better: use insertion sort
     return;
   }
   if (base == len || r-l <= 1) return;
   int start[max_val+1] = {0}, pos[max_val+1] = {0};
   start[0] = l;
-  rep(i,l,r) start[seq[i][base]+1]++;
+  rep(i,l,r) start[getdig(seq[i], base)+1]++;
   rep(i,1,max_val+1) start[i] += start[i-1], pos[i] = start[i];
   rep(c,0,max_val) {
     rep(i,start[c], start[c+1]) {
       for(;;) {
-        int d = seq[i][base];
+        int d = getdig(seq[i], base);
         if (d==c) break;
         int& j = pos[d];
         swap(seq[i], seq[j]);
         j++;
       }
     }
-    radix_sort_msb(seq,len,start[c],start[c+1],base+1);
+    radix_sort_msb(seq,getdig,len,start[c],start[c+1],base+1);
   }
 }
--- weight_balanced_tree.cpp
+++ weight_balanced_tree.cpp
@@ -1,52 +1,119 @@
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+typedef long long ll; typedef long double ld;
+#define rep(i,s,e) for (int i=(s);i<(e);++i)
+#define all(x) begin(x),end(x)
+#define clr(x,y) memset(x,y,sizeof x)
+#define contains(x,y) ((x).find(y)!=end(x))
+#define pb push_back
+#define Tu tuple
+#define mk make_pair
+#define mkt make_tuple
+#define fst first
+#define snd second
+#define _1 get<0>
+#define _2 get<1>
+#define _3 get<2>
+#define _4 get<3>
+#define sz(x) ((int)(x).size())
+
 // a BB[alpha] tree implementation that rebuilds entire subtrees if they get
 // unbalanced
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cassert>
 using namespace std;
 
-double alpha = 0.2;
+double alpha = 0.25;
 
+#define treesz(n) ((n) ? (n)->treesize : 0)
+#define valsz(n) ((n) ? (n)->valsize : 0)
 struct Node {
-  int val;
-  int size;
+  int val, count, valsize, treesize;
   Node *l, *r;
-  Node(int val_, int size_, Node* l_, Node* r_) : val(val_), size(size_), l(l_), r(r_) {}
+  Node(int val, int count, Node* l, Node* r) : val(val), count(count), l(l), r(r) {
+    maintain();
+  }
+  void maintain() {
+    valsize = count + valsz(l) + valsz(r);
+    treesize = 1 + treesz(l) + treesz(r);
+  }
+  bool balanced() {
+    return min(treesz(l), treesz(r)) >= alpha * (treesz(l) + treesz(r)) - 1;
+  }
 };
-
-#define NODESZ(n) ((n) ? (n)->size : 0)
-
-void dfs(Node *n, vector<int>& res) {
+vector<Node*> nodes;
+void dfs(Node *n) {
   if (!n) return;
-  dfs(n->l, res);
-  res.push_back(n->val);
-  dfs(n->r, res);
-  delete n;
+  dfs(n->l);
+  nodes.pb(n);
+  dfs(n->r);
 }
-
-Node* build(vector<int>& vals, int l, int r) {
-  if (r-l < 1) return 0;
-  int m = (l+r)/2;
-  return new Node(vals[m], r-l, build(vals, l, m), build(vals, m, r));
+Node* build(int l, int r) {
+  if (r < l) return 0;
+  int m = (l + r) / 2;
+  Node* res = new Node(nodes[m]->val, nodes[m]->count, build(l, m - 1), build(m + 1, r));
+  delete nodes[m];
+  return res;
 }
-
+int tot = 0;
 Node* maintain(Node* n) {
-  if (min(NODESZ(n->l), NODESZ(n->r)) >= floor(alpha * n->size))
-    return n;
-  vector<int> vals;
-  dfs(n, vals);
-  return build(vals, 0, vals.size());
+  n->maintain();
+  if (n->balanced()) return n;
+  nodes.clear();
+  dfs(n);
+  tot += nodes.size();
+  n = build(0, nodes.size() - 1);
+  assert(n->balanced());
+  return n;
+}
+void insert(Node*& n, int x, bool rem=0) {
+  if (!n && rem) return;
+  else if (!n) n = new Node(x, 1, 0, 0);
+  else if (x == n->val) n->count += rem ? -1 : 1, n->maintain();
+  else {
+    insert(x < n->val ? n->l : n->r, x, rem);
+    n = maintain(n);
+  }
+}
+void remove(Node*& n, int x) { return insert(n, x, 1); }
+
+Node *root = 0;
+
+// test helpers
+int height(Node *n) {
+  if (!n) return 0;
+  return 1 + max(height(n->l), height(n->r));
+}
+bool is_consistent(Node *n) {
+  if (!n) return 1;
+  if (treesz(n->l) + treesz(n->r) + 1 != n->treesize) return 0;
+  if (valsz(n->l) + valsz(n->r) + n->count != n->valsize) return 0;
+  if (!n->balanced()) return 0;
+  return is_consistent(n->l) && is_consistent(n->r);
+}
+void debug(Node* n, int ind = 0) {
+  rep(_,0,ind) cout << "  ";
+  if(!n) {cout << "NIL:0" << endl;return;}
+  cout << n->val << ":" << n->count <<" " << valsz(n) <<" " << is_consistent(n) << endl;
+  debug(n->l, ind + 1);
+  debug(n->r, ind + 1);
 }
 
-Node* insert(int x, Node* n) {
-  if (!n) return new Node(x, 1, 0, 0);
-  if (x <= n->val) n->l = insert(x, n->l);
-  else             n->r = insert(x, n->r);
-  return maintain(n);
-}
-
+int N = 1000000;
 int main() {
-  Node* root = 0;
-  for (int i = 1; i < 100000; ++i)
-    root = insert(i, root);
+  for (int i = 1; i <= N; ++i) insert(root, i);
+  for (int i = 1; i <= N; ++i) insert(root, i);
+  for (int i = 1; i <= N; ++i) remove(root, i);
+  //debug(root);
+  cout << "consistent: " << is_consistent(root) << endl;
+  cout << "heigth: " << height(root) << endl;
+  cout << "total rebuilt nodes: " << tot << endl;
+  //vals.clear();
+  //dfs(root);
+  //sort(all(vals));
+  //assert(vals.size() == N);
+  //for (int i = 0; i < N; ++i) assert(vals[i] == mk(i + 1, 1));
 }

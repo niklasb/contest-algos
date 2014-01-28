5bd2140fd2f7fff7df9bbf12b4cf2a8621dbd6e6
Niklas Baumstark <niklas.baumstark@gmail.com>
2014-01-28 16:29:26 -0500

    fix monoqueue

--- monotonic_queue.cpp
+++ monotonic_queue.cpp
@@ -1,15 +1,20 @@
+#include <deque>
+using namespace std;
+
 template <typename T>
 struct MinQueue {
-  deque<T> q, mono;
+  deque<pair<int, T> > q, mono;
+  int cnt = 0;
   void enqueue(T x) {
-    while (!mono.empty() && x <= mono.back()) mono.pop_back();
-    mono.push_back(x);
-    q.push_back(x);
+    while (!mono.empty() && x <= mono.back().second) mono.pop_back();
+    mono.push_back(make_pair(cnt,x));
+    q.push_back(make_pair(cnt,x));
+    cnt++;
   }
   void dequeue() {
-    if (mono.front() == q.front()) mono.pop_front();
+    if (mono.front().first == q.front().first) mono.pop_front();
     q.pop_front();
   }
-  T front() { return q.front(); }
-  T min() { return mono.front(); }
+  T front() { return q.front().second; }
+  T min() { return mono.front().second; }
 };

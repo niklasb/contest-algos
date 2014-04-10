bd0d739219193ae4b5f7dee35f1f9a307ddd1866
Niklas Baumstark <niklas.baumstark@gmail.com>
2014-04-10 11:47:35 -0400

    shorten dynamic convex hull code

--- convex_hull/dynamic.cpp
+++ convex_hull/dynamic.cpp
@@ -1,38 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
 const ll is_query = -(1LL<<62);
-struct HullDynamic { // will maintain upper hull for maximum
-    struct Line {
-        ll m, b;
-        mutable function<const Line*()> succ;
-        bool operator<(const Line& rhs) const {
-            if (rhs.b != is_query) return m < rhs.m;
-            const Line* s = succ();
-            if (!s) return 0;
-            ll x = rhs.m;
-            return b - s->b < (s->m - m) * x;
-        }
-    };
-    multiset<Line> lines;
-    typedef multiset<Line>::iterator It;
-    bool bad(It y) {
-        It z = next(y);
-        if (y == begin(lines)) {
-            if (z == end(lines)) return 0;
+struct Line {
+    ll m, b;
+    mutable function<const Line*()> succ;
+    bool operator<(const Line& rhs) const {
+        if (rhs.b != is_query) return m < rhs.m;
+        const Line* s = succ();
+        if (!s) return 0;
+        ll x = rhs.m;
+        return b - s->b < (s->m - m) * x;
+    }
+};
+struct HullDynamic : public multiset<Line> { // will maintain upper hull for maximum
+    bool bad(iterator y) {
+        auto z = next(y);
+        if (y == begin()) {
+            if (z == end()) return 0;
             return y->m == z->m && y->b <= z->b;
         }
-        It x = prev(y);
-        if (z == end(lines)) return y->m == x->m && y->b <= x->b;
+        auto x = prev(y);
+        if (z == end()) return y->m == x->m && y->b <= x->b;
         return (x->b - y->b)*(z->m - y->m) >= (y->b - z->b)*(y->m - x->m);
     }
-    void insert(ll m, ll b) {
-        It y = lines.insert({ m, b });
-        y->succ = [=] { return next(y) == lines.end() ? 0 : &*next(y); };
-        if (bad(y)) { lines.erase(y); return; }
-        while (next(y) != end(lines) && bad(next(y))) lines.erase(next(y));
-        while (y != begin(lines) && bad(prev(y))) lines.erase(prev(y));
+    void insert_line(ll m, ll b) {
+        auto y = insert({ m, b });
+        y->succ = [=] { return next(y) == end() ? 0 : &*next(y); };
+        if (bad(y)) { erase(y); return; }
+        while (next(y) != end() && bad(next(y))) erase(next(y));
+        while (y != begin() && bad(prev(y))) erase(prev(y));
     }
-    bool empty() { return lines.empty(); }
     ll eval(ll x) {
-        auto l = *lines.lower_bound((Line) { x, is_query });
+        auto l = *lower_bound((Line) { x, is_query });
         return l.m * x + l.b;
     }
 };
+
+//int main() {
+    //HullDynamicOld h;
+    //HullDynamic h2;
+    //for (int top : { 100, 1000, 1000000000 }) {
+        //int T = 10000;
+        //while(T--) {
+            //ll m = rand() % top;
+            //ll b = rand() % top;
+            //h.insert(m,b);
+            //h2.insert_line(m,b);
+            //int Q =1000;
+            //while(Q--) {
+                //ll x = rand() % top;
+                //assert(h.eval(x) == h2.eval(x));
+            //}
+        //}
+    //}
+//}

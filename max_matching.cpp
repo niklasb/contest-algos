21c682674bd977644d9a7e5a32590cf83a9dccef
Niklas Baumstark <niklas.baumstark@gmail.com>
2015-06-21 23:42:51 +0200

    update matching

--- max_matching.cpp
+++ max_matching.cpp
@@ -1,14 +1,6 @@
-int n;
-int adj[222][222];
-int a[222][222];
-const int MOD = 29989;
-int powmod(int a, int b) {
-    if (b==0) return 1;
-    if (b&1) return a * powmod(a,b-1) % MOD;
-    int rt = powmod(a,b/2);
-    return rt*rt % MOD;
-}
-int comp_rank() {
+const int N=200, MOD=1000000007, I=10;
+int n, adj[N][N], a[N][N];
+int rank() {
     int r = 0;
     rep(j,0,n) {
         int k = r;
@@ -17,18 +9,23 @@ int comp_rank() {
         swap(a[r], a[k]);
         int inv = powmod(a[r][j], MOD - 2);
         rep(i,j,n)
-            a[r][i] = a[r][i] * inv % MOD;
-        rep(u,r+1,n) rep(v,j+1,n)
-            a[u][v] = (a[u][v] - a[r][v] * a[u][j] % MOD + MOD) % MOD;
+            a[r][i] = 1LL * a[r][i] * inv % MOD;
+        rep(u,r+1,n) rep(v,j,n)
+            a[u][v] = (a[u][v] - 1LL * a[r][v] * a[u][j] % MOD + MOD) % MOD;
         ++r;
     }
     return r;
 }
+// failure probability = (n / MOD)^I
 int max_matching() {
-    rep(i,0,n) rep(j,0,i)
-        if (adj[i][j]) {
-            a[i][j] = rand() % (MOD - 1) + 1;
-            a[j][i] = MOD - a[i][j];
-        }
-    return comp_rank() >> 1;
+    int ans = 0;
+    rep(_,0,I) {
+        rep(i,0,n) rep(j,0,i)
+            if (adj[i][j]) {
+                a[i][j] = rand() % (MOD - 1) + 1;
+                a[j][i] = MOD - a[i][j];
+            }
+        ans = max(ans, rank()/2);
+    }
+    return ans;
 }

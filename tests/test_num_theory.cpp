1ddc4336b1ed314e7fc3e67645d08761c0f32624
Niklas Baumstark <niklas.baumstark@gmail.com>
2015-07-28 00:57:43 +0200

    update tests

--- tests/test_num_theory.cpp
+++ tests/test_num_theory.cpp
@@ -7,14 +7,14 @@ using ull=unsigned long long;
 #include "../number_theory.cpp"
 
 int main() {
-  for (int a = 0; a <= 100; ++a)
-    for (int m = 1; m <= 100; ++m) {
-      auto c = pow_cycle(a, m);
-      int i = c.first;
-      ll C = c.second;
-      for (int j = i; j <= i+100; ++j) {
-        assert(powmod(a, j, m) == powmod(a, j+C, m));
-        assert(powmod(a, j, m) == powmod(a, (j-i) % C + i, m));
-      }
+  for (int a = 1; a <= 10000; ++a) {
+    map<ll,int> facs;
+    factor(a, facs);
+    ll x = 1;
+    for (auto it: facs) {
+      assert(rabin(it.first));
+      x *= powmod(it.first, it.second, a+1);
     }
+    assert(x == a);
+  }
 }

1d5314a9d50a7c79416d5eab007df91f07c78348
Niklas Baumstark <niklas.baumstark@gmail.com>
2016-12-04 12:32:51 +0100

    mincostflow

--- mincostflow.cpp
+++ mincostflow.cpp
@@ -1,7 +1,7 @@
-const int MAXN = 10000, MAXC = 1<<29;
+const int MAXN = 10000, MAXC = 10000;
 struct edge { int dest, cap, cost, rev; };
 vector<edge> adj[MAXN];
-int dis[MAXN], source, target, iter, cap, cost;
+int dis[MAXN], cap[MAXN], source, target, iter, cost;
 edge* pre[MAXN];
 
 void addedge(int x, int y, int cap, int cost) {
@@ -14,15 +14,17 @@ bool spfa() { // optimization: use dijkstra here and do Johnson reweighting befo
   queue<int> q;
   pre[source] = pre[target] = 0;
   dis[source] = 0;
+  cap[source] = MAXC;
   q.emplace(source);
   while (!q.empty()) {
     int x = q.front(), d = dis[x];
     q.pop();
     for (auto& e : adj[x]) {
       int y = e.dest, w = d + e.cost;
-      if (e.cap < cap || dis[y] <= w) continue;
+      if (!e.cap || dis[y] <= w) continue;
       dis[y] = w;
       pre[y] = &e;
+      cap[y] = min(cap[x], e.cap);
       q.push(y);  // optimization: only push if not in queue yet
     }
   }
@@ -30,20 +32,17 @@ bool spfa() { // optimization: use dijkstra here and do Johnson reweighting befo
   if (!e) return 0;  // to minimize (cost, -flow): return also if dis[target] > 0
   while (e) {
     edge& rev = adj[e->dest][e->rev];
-    e->cap -= cap;
-    rev.cap += cap;
-    cost += cap * e->cost;
+    e->cap -= cap[target];
+    rev.cap += cap[target];
+    cost += cap[target] * e->cost;
     e = pre[rev.dest];
   }
   return 1;
 }
 
 pair<int,int> mincostflow(int S, int T) {
-  cap = MAXC, source = S, target = T, cost = 0;
+  source = S, target = T, cost = 0;
   int flow = 0;
-  while(cap) {
-    while(spfa()) flow += cap;
-    cap /= 2;
-  }
+  while(spfa()) flow += cap[target];
   return {flow, cost};
 }
